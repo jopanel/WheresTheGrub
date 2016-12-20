d457666dd643f3100ee6502f6d9b80a999aba2f4
hbj-joe <joe@DESIGNHBJ.local>
2016-12-19 16:08:57 -0800

    update

--- application/models/Vendor_model.php
+++ application/models/Vendor_model.php
@@ -8,12 +8,47 @@ class Vendor_model extends CI_Model {
 
     }
 
-    public function listAllPPC() {
+    /*
+    SESSION DATA:
 
+                    $this->session->set_userdata('uid', $query->row()->id);
+                    $this->session->set_userdata('fullname', $query->row()->fullname);
+                    $this->session->set_userdata('vendoremail', $email);
+                    $this->session->set_userdata('vendortoken', $sessiontoken);
+                    $this->session->set_userdata('vendorloggedin', '1');
+    */
+    public function addHistoryPPC($rid,$uid,$campaignid,$action=null) {
+        $sql = "INSERT INTO ppc_history (rid,action,created,user,campaignid) VALUES (".$this->db->escape($rid).",".$this->db->escape($action).", NOW() ,".$this->db->escape($uid).",".$this->db->escape($campaignid).")";
+        $this->db->query($sql);
+        return TRUE;
     }
 
-    public function createPPC() {
+    public function listAllPPC($rid) {
+        if (!isset($rid) || empty($rid)) { return false; }
+        $sql = "SELECT * FROM ppc_campaigns WHERE rid = ".$this->db->escape((int)$rid);
+        $query = $this->db->query($sql);
+        if ($query) {
+            if ($query->num_rows() > 0) {
+                return $query->result_array();
+            } else {
+                return array();
+            }
+        }
+    }
 
+    public function createPPC($rid,$data) { 
+        if ($this->session->userdata("uid")) { $uid = strip_tags($this->session->userdata("uid")); } else { return FALSE; }
+        if (!isset($data["budget"]) || empty($data["budget"])) { return FALSE; } else { $budget = $data["budget"]; }
+        if (!isset($data["name"]) || empty($data["name"])) { return FALSE; } else { $name = $data["name"]; }
+        $sql = "INSERT INTO ppc_campaigns (rid,budget,name,created,active) VALUES (".$this->db->escape($rid).",".$this->db->escape($budget).",".$this->db->escape($name).", NOW(), '0')";
+        $this->db->query($sql);
+        $sql2 = "SELECT MAX(id) as 'id' FROM ppc_campaigns WHERE rid = ".$this->db->escape($rid);
+        $query = $this->db->query($sql2);
+        if ($this->addHistoryPPC($rid,$uid,$query->row()->id,"New campaign created") == TRUE) {
+            return TRUE;
+        } else {
+            return FALSE;
+        }
     }
 
     public function editPPC() {
@@ -33,18 +68,23 @@ class Vendor_model extends CI_Model {
     }
 
     public function getBizReviewStats() {
-    
+        /*
+            Review Stats:
+            - how many reviews total
+            - how many reviews last 30 days
+            - how many reviews 1y
+            - average review rating
+            - average review power 
+        */
     }
 
-    public function getBizStats() {
-
+    public function getBizStats($rid) {
+        /*
+            foreach vendorstats_type get last 30 days, 1 year, and total
+        */
     }
 
     public function getPPCStats() {
-
-    }
-
-    public function getBizRatingStats() {
 
     }
 
